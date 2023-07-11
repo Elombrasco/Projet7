d86e3036ac8453f438901e0fd557badf1a523d98
Nyv <79833594+supernyv@users.noreply.github.com>
2023-07-12 00:40:57 +0100

    Update info.py

--- Dashboard/info.py
+++ Dashboard/info.py
@@ -1,6 +1,7 @@
-def return info(customer_id)
-gender = ""
-if df.loc[customer_id, "CODE_GENDER_M"] == 1:
-  gender = "Male"
-else:
-  gender = "Female"
+def return info(customer_id):
+  gender = ""
+  Age = ""
+  if df.loc[customer_id, "CODE_GENDER_M"] == 1:
+    gender = "Male"
+  else:
+    gender = "Female"
