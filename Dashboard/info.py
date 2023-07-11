7790460e039e5f084de1b43d9b097d291bf48a99
Nyv <79833594+supernyv@users.noreply.github.com>
2023-07-12 00:45:35 +0100

    Update info.py

--- Dashboard/info.py
+++ Dashboard/info.py
@@ -1,7 +1,16 @@
-def return info(customer_id):
-  gender = ""
-  Age = ""
-  if df.loc[customer_id, "CODE_GENDER_M"] == 1:
-    gender = "Male"
-  else:
-    gender = "Female"
+import pandas as pd
+
+
+#Load the data and model
+df = pd.read_csv('data/X_sample.csv', index_col="SK_ID_CURR", encoding="utf-8")
+
+def info(customer_id):
+    gender = ""
+    Age = ""
+    if df.loc[customer_id, "CODE_GENDER_M"] == 1:
+        gender = "Male"
+    else:
+        gender = "Female"
+    return gender, Age
+
+print(info(280998))
