f96394b51d0b7cf362328f42e65da6e8d2370a64
Elombrasco <88459096+Elombrasco@users.noreply.github.com>
2023-07-15 00:37:49 +0200

    Update info.py

--- Dashboard/info.py
+++ Dashboard/info.py
@@ -14,3 +14,10 @@ def info(customer_id):
     return gender, Age
 
 print(info("280998"))
+
+1. `sorted_idx = np.argsort(lgbm.feature_importances_)[::-1]` : Cette ligne calcule les indices des fonctionnalités (features) dans l'ordre décroissant de leur importance. Il utilise `np.argsort` pour obtenir les indices triés en fonction des valeurs d'importance renvoyées par `lgbm.feature_importances_`. Ensuite, `[::-1]` inverse l'ordre pour avoir une liste en ordre décroissant.
+
+2. `for index in sorted_idx:` : Cette ligne itère sur les indices triés des fonctionnalités.
+
+3. `print([X_data.columns[index], lgbm.feature_importances_[index]])` : Cette ligne imprime le nom de la fonctionnalité (colonne) correspondante, ainsi que son importance renvoyée par `lgbm.feature_importances_[index]`. `X_data.columns[index]` renvoie le nom de la colonne associée à l'indice `index`, et `lgbm.feature_importances_[index]` renvoie l'importance de cette fonctionnalité selon le modèle LightGBM.
+
